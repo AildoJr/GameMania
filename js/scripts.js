7d3394eca1ce737fbc24605db7fea93b3a06ded1
Aildo Jr <afsjr_1989@hotmail.com>
2022-10-15 15:51:41 -0300

    Função MostrarMenu adicionada no arquivo scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,13 @@
 //console.log('Mensagem enviada pelo console');
 
 //var numero = 44;
+function mostrarMenu()
+        {
+            let menu = document.getElementById("menu-sec");
+            menu.classList.toggle("mostrarMenu");
+            // if (menu.style.display == 'flex'){
+            //     menu.style.display = 'none';
+            // }else{
+            //     menu.style.display = 'flex';
+            // }
+        }
